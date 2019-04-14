38ce79c48760d8d6522b35b4717f8027ae9b1226
Manuel Alonso <manueldavid@live.com.mx>
2019-04-13 20:53:57 -0500

    Servicios

--- js/rutas.js
+++ js/rutas.js
@@ -34,7 +34,7 @@ app.config(function ($routeProvider) {
 
 
 app.controller('homeController', function ($scope) {
-    $scope.message = 'El farolito!';
+    $scope.message = 'Te invitamos a conocer un lugar de descanso y relajación en el pueblo mágico, donde a través de nuestras bellas instalaciones y una atención personalizada, te consentiremos dándote momentos coloridos, llenos de sabores  y experiencias que caracterizan nuestra cultura Mexicana.';
 });
 app.controller('aboutController', function ($scope) {
     $scope.message = 'Descubre más sobre nosotros';
